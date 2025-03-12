cb4aeaf46e1cf3dc2a7a59c83bd2f5f0eca8b65e
Sophie King <102487877+theskywillbeblue@users.noreply.github.com>
2025-03-12 16:31:37 +0000

    add Supabase connection, editing connection file & package.json

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,9 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("No PGDATABASE configured")
+    throw new Error("No PGDATABASE configured or DATABASE_URL not set")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
@@ -16,8 +15,10 @@ const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config.max = 3;
 }
+
+const db = new Pool(config);
 
 
 module.exports = new Pool(config);
